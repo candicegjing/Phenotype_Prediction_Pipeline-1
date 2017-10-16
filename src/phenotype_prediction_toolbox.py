0e8bb125b4ae3a1367ac41bf946ccc3a3494bd1a
Nahil Sobh <nahil.sobh@gmail.com>
2017-10-16 15:57:33 -0500

    refactored the toolbox

--- src/phenotype_prediction_toolbox.py
+++ src/phenotype_prediction_toolbox.py
@@ -7,43 +7,53 @@ import knpackage.toolbox as kn
 
 def run_lasso_predict(run_parameters):
 
-    gene_samples_train_df      = kn.get_spreadsheet_df(run_parameters['spreadsheet_name_full_path'     ])
-    response_train_df          = kn.get_spreadsheet_df(run_parameters['response_name_full_path'        ])
-    gene_samples_test_df       = kn.get_spreadsheet_df(run_parameters['test_spreadsheet_name_full_path'])
+    genes_train_df     = kn.get_spreadsheet_df(run_parameters['spreadsheet_name_full_path'     ])
+    response_train_df  = kn.get_spreadsheet_df(run_parameters['response_name_full_path'        ])
 
-    response_test_sample_names = list(gene_samples_test_df.columns)
+    genes_test_df      = kn.get_spreadsheet_df(run_parameters['test_spreadsheet_name_full_path'])
+    columns_header     = list(genes_test_df.columns)
 
-    reg_moE                    = linear_model.Lasso()
-    response_predict           = reg_moE.fit( gene_samples_train_df.transpose().values
-                                            , response_train_df.values[0]).predict(gene_samples_test_df.transpose().values)
+    genes_train_mat    = genes_train_df.values
+    response_train_mat = response_train_df.values[0]
+    genes_test_mat     = genes_test_df.values
 
-    predict_df                 = pd.DataFrame(response_predict.T, index=response_test_sample_names, columns=['predict'])
+    reg_moE            = linear_model.Lasso()
+    response_predict   = reg_moE.fit( genes_train_mat.T, response_train_mat).predict(genes_test_mat.T)
+
+    predict_df         = pd.DataFrame(response_predict.T, index=columns_header, columns=['predict'])
 
     write_predict_data(predict_df, run_parameters)
     
 
 def run_elastic_predict(run_parameters):
 
-    gene_samples_train_df      = kn.get_spreadsheet_df(run_parameters['spreadsheet_name_full_path'     ])
-    response_train_df          = kn.get_spreadsheet_df(run_parameters['response_name_full_path'        ])
-    gene_samples_test_df       = kn.get_spreadsheet_df(run_parameters['test_spreadsheet_name_full_path'])
+    genes_train_df    = kn.get_spreadsheet_df(run_parameters['spreadsheet_name_full_path'     ])
+    response_train_df = kn.get_spreadsheet_df(run_parameters['response_name_full_path'        ])
 
-    response_test_sample_names = list(gene_samples_test_df.columns)
+    genes_test_df     = kn.get_spreadsheet_df(run_parameters['test_spreadsheet_name_full_path'])
+    columns_header    = list(genes_test_df.columns)
 
-    reg_moE                    = linear_model.ElasticNetCV()
-    response_predict           = reg_moE.fit( gene_samples_train_df.transpose().values
-                                            , response_train_df.values[0]).predict(gene_samples_test_df.transpose().values)
+    genes_train_mat    = genes_train_df.values
+    response_train_mat = response_train_df.values[0]
+    genes_test_mat     = genes_test_df.values
 
-    predict_df                 = pd.DataFrame(response_predict.T, index=response_test_sample_names, columns=['predict'])
+    reg_moE            = linear_model.ElasticNetCV()
+    response_predict   = reg_moE.fit( genes_train_mat.T, response_train_mat).predict(genes_test_mat.T)
+
+    predict_df         = pd.DataFrame(response_predict.T, index=columns_header, columns=['predict'])
 
     write_predict_data(predict_df, run_parameters)
 
 
 def write_predict_data(predict_df, run_parameters):
 
-    deNada, output_file_name = os.path.split(run_parameters['test_spreadsheet_name_full_path'])
-    output_file_name, deNada = os.path.splitext(output_file_name)
-    output_file_name         = os.path.join(run_parameters['results_directory'], output_file_name)
-    output_file_name         = kn.create_timestamped_filename(output_file_name) + '.tsv'
+    test_spreadsheet_name_full_path = run_parameters['test_spreadsheet_name_full_path']
+    results_directory               = run_parameters['results_directory']
+    _, output_file_name             = os.path.split(test_spreadsheet_name_full_path)
+
+    output_file_name, _             = os.path.splitext(output_file_name)
+    output_file_name                = os.path.join(results_directory, output_file_name)
+    output_file_name                = kn.create_timestamped_filename(output_file_name) + '.tsv'
 
     predict_df.to_csv(output_file_name, sep='\t', header=True, index=True)
+
