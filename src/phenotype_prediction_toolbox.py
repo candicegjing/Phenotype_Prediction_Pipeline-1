69da2f8b215c9114075a8286b6db287ed812ac74
Nahil Sobh <nahil.sobh@gmail.com>
2017-10-17 12:25:53 -0500

    standarization of code

--- src/phenotype_prediction_toolbox.py
+++ src/phenotype_prediction_toolbox.py
@@ -7,41 +7,45 @@ import knpackage.toolbox as kn
 
 def run_lasso_predict(run_parameters):
 
-    genes_train_df     = kn.get_spreadsheet_df(run_parameters['spreadsheet_name_full_path'     ])
-    response_train_df  = kn.get_spreadsheet_df(run_parameters['response_name_full_path'        ])
+    gene_file = run_parameters['spreadsheet_name_full_path'     ]
+    sign_file = run_parameters['response_name_full_path'        ]
+    test_file = run_parameters['test_spreadsheet_name_full_path']
 
-    genes_test_df      = kn.get_spreadsheet_df(run_parameters['test_spreadsheet_name_full_path'])
-    columns_header     = list(genes_test_df.columns)
+    gene_df   = kn.get_spreadsheet_df(gene_file)
+    sign_df   = kn.get_spreadsheet_df(sign_file)
+    test_df   = kn.get_spreadsheet_df(test_file)
 
-    genes_train_mat    = genes_train_df.values
-    response_train_mat = response_train_df.values[0]
-    genes_test_mat     = genes_test_df.values
+    row_names = test_df.columns
 
-    reg_moE            = linear_model.Lasso()
-    response_predict   = reg_moE.fit( genes_train_mat.T, response_train_mat).predict(genes_test_mat.T)
+    gene_mat  = gene_df.values
+    sign_mat  = sign_df.values[0]
+    test_mat  = test_df.values
 
-    predict_df         = pd.DataFrame(response_predict.T, index=columns_header, columns=['predict'])
-
+    reg_model          = linear_model.Lasso()
+    response_predict   = reg_model.fit( gene_mat.T, sign_mat).predict(test_mat.T)
+    predict_df         = pd.DataFrame(response_predict.T, index=row_names, columns=['predict'])
     write_predict_data(predict_df, run_parameters)
     
 
 def run_elastic_predict(run_parameters):
 
-    genes_train_df    = kn.get_spreadsheet_df(run_parameters['spreadsheet_name_full_path'     ])
-    response_train_df = kn.get_spreadsheet_df(run_parameters['response_name_full_path'        ])
+    gene_file = run_parameters['spreadsheet_name_full_path'     ]
+    sign_file = run_parameters['response_name_full_path'        ]
+    test_file = run_parameters['test_spreadsheet_name_full_path']
 
-    genes_test_df     = kn.get_spreadsheet_df(run_parameters['test_spreadsheet_name_full_path'])
-    columns_header    = list(genes_test_df.columns)
+    gene_df   = kn.get_spreadsheet_df(gene_file)
+    sign_df   = kn.get_spreadsheet_df(sign_file)
+    test_df   = kn.get_spreadsheet_df(test_file)
 
-    genes_train_mat    = genes_train_df.values
-    response_train_mat = response_train_df.values[0]
-    genes_test_mat     = genes_test_df.values
+    row_names = test_df.columns
 
-    reg_moE            = linear_model.ElasticNetCV()
-    response_predict   = reg_moE.fit( genes_train_mat.T, response_train_mat).predict(genes_test_mat.T)
+    gene_mat  = gene_df.values
+    sign_mat  = sign_df.values[0]
+    test_mat  = test_df.values
 
-    predict_df         = pd.DataFrame(response_predict.T, index=columns_header, columns=['predict'])
-
+    reg_model         = linear_model.ElasticNetCV()
+    response_predict  = reg_model.fit( gene_mat.T, sign_mat).predict(test_mat.T)
+    predict_df        = pd.DataFrame(response_predict.T, index=row_names, columns=['predict'])
     write_predict_data(predict_df, run_parameters)
 
 
