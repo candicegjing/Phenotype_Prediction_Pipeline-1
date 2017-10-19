5325062e7a3bb35bae789786169185821739ea3a
XICHEN24 <xichen24@illinois.edu>
2017-10-18 20:44:28 -0700

    add method lassoCV, elasticNetCV and verification

--- src/phenotype_prediction_toolbox.py
+++ src/phenotype_prediction_toolbox.py
@@ -1,6 +1,8 @@
 
 import os
 import pandas as pd
+import numpy as np
+import pickle
 from   sklearn import linear_model
 
 import knpackage.toolbox as kn
@@ -21,8 +23,19 @@ def run_lasso_predict(run_parameters):
     sign_mat  = sign_df.values[0]
     test_mat  = test_df.values
 
-    reg_model          = linear_model.Lasso()
-    response_predict   = reg_model.fit( gene_mat.T, sign_mat).predict(test_mat.T)
+    min_alpha     = run_parameters['min_alpha']
+    max_alpha     = run_parameters['max_alpha']
+    n_alpha       = run_parameters['n_alpha']
+    intercept     = run_parameters['fit_intercept']
+    normalization = run_parameters['normalize']
+    alpha_grid    = np.linspace(min_alpha, max_alpha, num=n_alpha)
+    reg_model     = linear_model.LassoCV(
+        alphas=alpha_grid, fit_intercept=intercept, normalize=normalization, max_iter=1000, cv=5)
+
+    reg_model.fit( gene_mat.T, sign_mat)
+    filename      = os.path.join(run_parameters['results_directory'], 'lasso_model.pkl') 
+    pickle.dump(reg_model, open(filename, 'wb'))
+    response_predict   = reg_model.predict(test_mat.T)
     predict_df         = pd.DataFrame(response_predict.T, index=row_names, columns=['predict'])
     write_predict_data(predict_df, run_parameters)
     
@@ -43,8 +56,28 @@ def run_elastic_predict(run_parameters):
     sign_mat  = sign_df.values[0]
     test_mat  = test_df.values
 
-    reg_model         = linear_model.ElasticNetCV()
-    response_predict  = reg_model.fit( gene_mat.T, sign_mat).predict(test_mat.T)
+    eps       = run_parameters['eps']    
+    min_alpha = run_parameters['min_alpha']
+    max_alpha = run_parameters['max_alpha']
+    n_alpha   = run_parameters['n_alpha']
+    min_l1    = run_parameters['min_l1']
+    max_l1    = run_parameters['max_l1']
+    n_l1      = run_parameters['n_l1']
+    intercept = run_parameters['fit_intercept']
+    normalize = run_parameters['normalize']
+    alpha_grid= np.linspace(min_alpha, max_alpha, num=n_alpha)
+    l1_grid   = np.linspace(min_l1, max_l1, num=n_l1)
+
+
+    reg_model = linear_model.ElasticNetCV(
+        l1_ratio=l1_grid, alphas=alpha_grid, fit_intercept=intercept, eps = eps,\
+        normalize=normalize, max_iter=1000, cv=5)
+
+    reg_model.fit(gene_mat.T, sign_mat)
+
+    filename      = os.path.join(run_parameters['results_directory'], 'elastic_net_model.pkl') 
+    pickle.dump(reg_model, open(filename, 'wb'))
+    response_predict = reg_model.predict(test_mat.T)
     predict_df        = pd.DataFrame(response_predict.T, index=row_names, columns=['predict'])
     write_predict_data(predict_df, run_parameters)
 
@@ -53,11 +86,12 @@ def write_predict_data(predict_df, run_parameters):
 
     test_spreadsheet_name_full_path = run_parameters['test_spreadsheet_name_full_path']
     results_directory               = run_parameters['results_directory']
+    method                          = run_parameters['method']
     _, output_file_name             = os.path.split(test_spreadsheet_name_full_path)
 
     output_file_name, _             = os.path.splitext(output_file_name)
-    output_file_name                = os.path.join(results_directory, output_file_name)
+    output_file_name                = os.path.join(results_directory, output_file_name + '_' + method)
     output_file_name                = kn.create_timestamped_filename(output_file_name) + '.tsv'
 
-    predict_df.to_csv(output_file_name, sep='\t', header=True, index=True)
+    predict_df.to_csv(output_file_name, sep='\t', header=True, index=True, float_format='%g')
 
